6b51307f95ecbe8b99ee3aa3b850b98e542fa859
Luke <lukehi811@gmail.com>
2026-05-27 22:16:23 -0600

    Allow overwriting admin content blob

--- lib/site-content.ts
+++ lib/site-content.ts
@@ -88,6 +88,7 @@ async function readSiteContentFromBlob(): Promise<Partial<SiteContent> | null> {
 async function writeSiteContentToBlob(content: SiteContent) {
   await put(blobPathname, JSON.stringify(content, null, 2), {
     access: 'private',
+    allowOverwrite: true,
     addRandomSuffix: false,
     contentType: 'application/json'
   });
